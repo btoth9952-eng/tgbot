a1c5157eb5f84dcc3ed4672555a5782eafc74c80
btoth9952-eng <b.toth9952@gmail.com>
2026-05-31 20:20:42 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ logger = logging.getLogger(__name__)
 
 # ─── Konfiguráció ───────────────────────────────────────────────────────────
 
-BOT_TOKEN   = os.environ["8857627658:AAFBgh02TpMyXfuaHdS3hFqtG02ZtBucPhM"]
+BOT_TOKEN   = os.environ["8611492822:AAF2ENEWpYT_A2BveQt3cnGVZvRirdKatp8"]
 ADMIN_ID    = int(os.environ["7819220971"])
 CHANNEL     = os.environ["@kasziradar"]            # pl. "@csatornad"
 HEALTH_PORT = int(os.environ.get("PORT", 8000))
